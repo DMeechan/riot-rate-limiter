2c13d6778e54df7dc5080c5406181bde521870cb
Daniel Meechan <git@public.dmeechan.com>
2022-02-13 05:05:21 +0000

    Add dist folder
    To fix importing from GitHub

---
+++ dist/cjs/index.d.ts
@@ -0,0 +1,24 @@
+import Bottleneck from "bottleneck";
+import { ConstructorParams, ExecuteParameters, PlatformId, METHODS, HOST } from "./@types";
+import { extractMethod, extractRegion } from "./extractor";
+export { extractMethod, extractRegion, METHODS, HOST, PlatformId };
+export declare class RiotRateLimiter {
+    readonly configuration: {
+        debug: boolean;
+        concurrency: number;
+        retryAfterDefault: number;
+        retryCount: number;
+        datastore: "local";
+    };
+    readonly rateLimiters: {
+        [key: string]: any;
+    };
+    constructor(config?: ConstructorParams);
+    private checkConcurrency;
+    private getRateLimiterOptions;
+    private setupRateLimiters;
+    private updateRateLimiters;
+    private syncRateLimiters;
+    execute(req: ExecuteParameters, jobOptions?: Bottleneck.JobOptions): Promise<any>;
+    private executeRequest;
+}
